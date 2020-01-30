7b0b2f1dd57c31536f2ee09c7e563a2383502b06
Ataer Caner Celik <caner.celik@sahibinden.com>
2020-01-30 13:03:59 +0300

    no message

---
+++ SahibindenCourseProject3/app/src/main/java/com/sahibindencourseproject3/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.sahibindencourseproject3
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
