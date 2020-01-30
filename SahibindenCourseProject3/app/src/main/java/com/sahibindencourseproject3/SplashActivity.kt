4d8a687c4475428ae3ec8454f13c631d90dcebcd
Ataer Caner Celik <caner.celik@sahibinden.com>
2020-01-30 13:41:02 +0300

    no message

---
+++ SahibindenCourseProject3/app/src/main/java/com/sahibindencourseproject3/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.sahibindencourseproject3
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+ * Created by Ataer Caner on 2020-01-30.
+Copyright (c) 2020 sahibinden. All rights reserved.
+ *
+ */
+
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activiy_splash)
+        navigateToMain()
+    }
+
+    fun navigateToMain() {
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+        },1500)
+    }
+
+}
